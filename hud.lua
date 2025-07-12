8e9b4010a5c5ed9f147db1547d0e1e6a23bfe11a
DylanTaylor1 <taylor.dylan173@gmail.com>
2025-07-12 10:01:22 +0200

    Add Multiplayer Glasses Support

--- hud.lua
+++ hud.lua
@@ -3,13 +3,17 @@ local config    = require('config')
 local events    = require('events')
 local component = require('component')
 local term      = require('term')
-local glasses   = component.glasses
 local lsc       = component.gt_machine
 local last      = 0
+local glasses   = {}
 
-glasses.removeAll()
+-- Initialization
 term.clear()
 graphics.fox()
+
+for address in component.list('glasses') do
+	table.insert(glasses, component.proxy(component.get(address)))
+end
 
 -- Configure Graphics
 local l = config.length
@@ -22,19 +26,23 @@ if not config.fullscreen then
   y = y - graphics.calcOffset(config.GUIscale)
 end
 
--- Draw Static Shapes
-graphics.quad(glasses, {0, y-b1}, {3.5*h+l+b2+1, y-b1}, {2.5*h+l+1, y-b1-h-b2}, {0, y-b1-h-b2}, config.borderColor)
-graphics.quad(glasses, {0, y}, {3.5*h+l+b2+1, y}, {3.5*h+l+b2+1, y-b1}, {0, y-b1}, config.borderColor)
-graphics.quad(glasses, {3.5*h, y-b1}, {3.5*h+l, y-b1}, {2.5*h+l, y-b1-h}, {2.5*h, y-b1-h}, config.secondaryColor)
+for i=1, #glasses do
+  glasses[i].removeAll()
 
--- Draw Energy Bar
-energyBar = graphics.quad(glasses, {b2+3.25*h, y-b1}, {b2+3.25*h, y-b1}, {b2+2.25*h, y-b1-h}, {b2+2.25*h, y-b1-h}, config.primaryColor)
-textPercent = graphics.text(glasses, 'X.X%', {0.5*h, y-b1-h/1.8-config.fontSize}, config.fontSize, config.primaryColor)
+  -- Draw Static Shapes
+  graphics.quad(glasses[i], {0, y-b1}, {3.5*h+l+b2+1, y-b1}, {2.5*h+l+1, y-b1-h-b2}, {0, y-b1-h-b2}, config.borderColor)
+  graphics.quad(glasses[i], {0, y}, {3.5*h+l+b2+1, y}, {3.5*h+l+b2+1, y-b1}, {0, y-b1}, config.borderColor)
+  graphics.quad(glasses[i], {3.5*h, y-b1}, {3.5*h+l, y-b1}, {2.5*h+l, y-b1-h}, {2.5*h, y-b1-h}, config.secondaryColor)
 
--- Draw Optional Values
-textCurr = graphics.text(glasses, '', {b2+3.25*h+1, y-b1-h/2-config.fontSize}, config.fontSize/1.3, config.textColor)
-textMax = graphics.text(glasses, '', {-2.25*h+l, y-b1-h/2-config.fontSize}, config.fontSize/1.3, config.textColor)
-textMaintenance = graphics.text(glasses, '', {b2, y-b1-b2-h-3*config.fontSize}, config.fontSize, config.issueColor)
+  -- Draw Energy Bar
+  glasses[i].energyBar = graphics.quad(glasses[i], {b2+3.25*h, y-b1}, {b2+3.25*h, y-b1}, {b2+2.25*h, y-b1-h}, {b2+2.25*h, y-b1-h}, config.primaryColor)
+  glasses[i].textPercent = graphics.text(glasses[i], 'X.X%', {0.5*h, y-b1-h/1.8-config.fontSize}, config.fontSize, config.primaryColor)
+
+  -- Draw Optional Values
+  glasses[i].textCurr = graphics.text(glasses[i], '', {b2+3.25*h+1, y-b1-h/2-config.fontSize}, config.fontSize/1.3, config.textColor)
+  glasses[i].textMax = graphics.text(glasses[i], '', {-2.25*h+l, y-b1-h/2-config.fontSize}, config.fontSize/1.3, config.textColor)
+  glasses[i].textMaintenance = graphics.text(glasses[i], '', {b2, y-b1-b2-h-3*config.fontSize}, config.fontSize, config.issueColor)
+end
 
 -- Stand Ready for Exit Command
 events.hookEvents()
@@ -56,19 +64,7 @@ while true do
 
   local percentage = math.min(power / capacity, 1)
 
-  -- Adjust Energy Bar
-  energyBar.setVertex(2, b2+3.25*h+l*percentage, y-b1)
-  energyBar.setVertex(3, b2+2.25*h+l*percentage, y-b1-h)
-
-  if percentage > 0.999 then
-    textPercent.setText('100%')
-    textPercent.setPosition(b2+2.1*h-2*config.fontSize*(#textPercent.getText()), y-b1-h/1.8-config.fontSize)
-  else
-    textPercent.setText(string.format('%.1f%%', percentage*100))
-    textPercent.setPosition(b2+2*h-2*config.fontSize*(#textPercent.getText()-1), y-b1-h/1.8-config.fontSize)
-  end
-
-  -- Adjust Optional Values
+  -- Adjust Values
   if config.showCurrentEU then
     if config.metric then
       curr = graphics.metricParser(power)
@@ -76,33 +72,48 @@ while true do
       curr = graphics.scientificParser(power)
     end
   else
-    curr = ""
+    curr = ''
   end
 
   if config.showRate then
     rate = graphics.calcRate(percentage, last, config.rateThreshold)
     last = percentage
   else
-    rate = ""
+    rate = ''
   end
 
-  textCurr.setText(curr .. " " .. rate)
+  for i=1, #glasses do
 
-  if config.showMaxEU then
-    if config.metric then
-      textMax.setText(graphics.metricParser(capacity))
-      textMax.setPosition(2.25*h+l-1.5*config.fontSize*(#textMax.getText()-1), y-b1-h/2-config.fontSize)
+    -- Adjust Energy Bar
+    glasses[i].energyBar.setVertex(2, b2+3.25*h+l*percentage, y-b1)
+    glasses[i].energyBar.setVertex(3, b2+2.25*h+l*percentage, y-b1-h)
+
+    if percentage > 0.999 then
+      glasses[i].textPercent.setText('100%')
+      glasses[i].textPercent.setPosition(b2+2.1*h-2*config.fontSize*(#textPercent.getText()), y-b1-h/1.8-config.fontSize)
     else
-      textMax.setText(graphics.scientificParser(capacity))
-      textMax.setPosition(2.25*h+l-1.5*config.fontSize*(#textMax.getText()-1), y-b1-h/2-config.fontSize)
+      glasses[i].textPercent.setText(string.format('%.1f%%', percentage*100))
+      glasses[i].textPercent.setPosition(b2+2*h-2*config.fontSize*(#textPercent.getText()-1), y-b1-h/1.8-config.fontSize)
     end
-  end
 
-  -- Detect Maintenance Issues
-  if #scan[17] < 43 then
-    textMaintenance.setText('Has Problems!')
-  else
-    textMaintenance.setText('')
+    glasses[i].textCurr.setText(curr .. ' ' .. rate)
+
+    if config.showMaxEU then
+      if config.metric then
+        glasses[i].textMax.setText(graphics.metricParser(capacity))
+        glasses[i].textMax.setPosition(2.25*h+l-1.5*config.fontSize*(#textMax.getText()-1), y-b1-h/2-config.fontSize)
+      else
+        glasses[i].textMax.setText(graphics.scientificParser(capacity))
+        glasses[i].textMax.setPosition(2.25*h+l-1.5*config.fontSize*(#textMax.getText()-1), y-b1-h/2-config.fontSize)
+      end
+    end
+
+    -- Detect Maintenance Issues
+    if #scan[17] < 43 then
+      glasses[i].textMaintenance.setText('Has Problems!')
+    else
+      glasses[i].textMaintenance.setText('')
+    end
   end
 
   -- Terminal Condition
@@ -115,4 +126,6 @@ while true do
 end
 
 events.unhookEvents()
-glasses.removeAll()
+for i=1, #glasses do
+  glasses[i].removeAll()
+end
